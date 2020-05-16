23c70ad6632ab4ac5f2ce27d19aae87827c81e4d
pluiel <pluiel@outlook.com>
2020-05-16 02:52:10 -0700

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Yuka Uematsu on 2020/05/16.
+//  Copyright © 2020 Uematsu Well. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
